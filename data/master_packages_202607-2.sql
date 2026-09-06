e0e01232c2e814eaf10ba79a5a94155435fb7ca4
Augusto Beiro <augusto.beiro@gmail.com>
2026-09-06 17:05:36 +0200

    Remove existing constraints from master_packages before adding primary key

--- data/master_packages_202607-2.sql
+++ data/master_packages_202607-2.sql
@@ -3,6 +3,24 @@
 --
 -- Data for Name: master_packages; Type: TABLE DATA; Schema: public; Owner: dwemer
 --
+
+DO $$
+DECLARE
+    constraint_record record;
+BEGIN
+    FOR constraint_record IN
+        SELECT conname
+        FROM pg_constraint
+        WHERE conrelid = 'public.master_packages'::regclass
+    LOOP
+        EXECUTE format(
+            'ALTER TABLE public.master_packages DROP CONSTRAINT IF EXISTS %I',
+            constraint_record.conname
+        );
+    END LOOP;
+END
+$$;
+
 ALTER TABLE ONLY public.master_packages
     ADD CONSTRAINT master_packages_pk PRIMARY KEY (formid);
 
