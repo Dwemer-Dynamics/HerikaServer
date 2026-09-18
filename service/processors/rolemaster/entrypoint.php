79caadf5446fc82a049315b1b508fef3d40d42af
Augusto Beiro <augusto.beiro@gmail.com>
2026-09-18 21:33:43 +0200

    Add Hypnosis mode to chim_modes and refactor entrypoint for consistency

--- service/processors/rolemaster/entrypoint.php
+++ service/processors/rolemaster/entrypoint.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 
-$GLOBALS["TASKS"]["rolemaster"]=[];
-$GLOBALS["TASKS"]["rolemaster"]["fn"]=function() {
+$GLOBALS["TASKS"]["rolemaster"] = [];
+$GLOBALS["TASKS"]["rolemaster"]["fn"] = function () {
 
     $enginePath = $GLOBALS["ENGINE_ROOT"];
 
@@ -9,7 +9,7 @@ $GLOBALS["TASKS"]["rolemaster"]["fn"]=function() {
     $GLOBALS["FUNCTION_PARM_INSPECT"] = [];
     $GLOBALS["FUNCTION_PARM_MOVETO"] = [];
     $GLOBALS["F_NAMES"] = [];
-    
+
     require_once($enginePath . "lib/runtime_bootstrap.php");
     chimRuntimeBootstrapIfNeeded($enginePath, [
         'load_general_settings' => true,
@@ -21,61 +21,68 @@ $GLOBALS["TASKS"]["rolemaster"]["fn"]=function() {
     if (isset($GLOBALS["CORE_LANG"])) {
         $GLOBALS["CORE_LANG"] = '';
     }
-    if (!isset($GLOBALS["db"])) { $GLOBALS["db"] = new sql(); }
+    if (!isset($GLOBALS["db"])) {
+        $GLOBALS["db"] = new sql();
+    }
     require_once($enginePath . "prompts/command_prompt.php");
     require_once($enginePath . "lib/chat_helper_functions.php");
     require_once($enginePath . "lib/data_functions.php");
     require_once($enginePath . "lib/rolemaster_helpers.php");
     require_once($enginePath . "lib/rolemaster_bored.php");
 
-    
-    SaveOriginalHerikaName(); 
-    $GLOBALS["HERIKA_NAME"]="(actor)";
-    
+
+    SaveOriginalHerikaName();
+    $GLOBALS["HERIKA_NAME"] = "(actor)";
+
     require($enginePath . "functions/functions.php");
 
     // Make functions.php data global
 
-    
-    $GLOBALS["FUNCTIONS_ARE_ENABLED"]=false;
 
-    $GLOBALS["CURRENT_CONNECTOR"]=$GLOBALS["CONNECTORS_DIARY"];
+    $GLOBALS["FUNCTIONS_ARE_ENABLED"] = false;
+
+    $GLOBALS["CURRENT_CONNECTOR"] = $GLOBALS["CONNECTORS_DIARY"];
 
 
-    if (!isset($GLOBALS["db"])) { $GLOBALS["db"] = new sql(); }
+    if (!isset($GLOBALS["db"])) {
+        $GLOBALS["db"] = new sql();
+    }
     if (!$GLOBALS["db"]) {
         throw new Exception("Database connection established, but an error occurred during initialization.");
     }
 
     // Some functions need this setted 
     //$res=$GLOBALS["db"]->fetchAll("select max(gamets)+1 as gamets,max(ts)+1 as ts  from eventlog order by gamets desc limit 1 offset 0");
-    $res=$GLOBALS["db"]->fetchAll("SELECT max(gamets)+1 as gamets, max(ts)+1 as ts FROM eventlog "); // without order is faster, pgsql can use a better exec plan
+    $res = $GLOBALS["db"]->fetchAll("SELECT max(gamets)+1 as gamets, max(ts)+1 as ts FROM eventlog "); // without order is faster, pgsql can use a better exec plan
     if ($res) {
-        $GLOBALS["gameRequest"]=["inputtext"];
-        $GLOBALS["gameRequest"][2]=$res[0]["gamets"]+1;
+        $GLOBALS["gameRequest"] = ["inputtext"];
+        $GLOBALS["gameRequest"][2] = $res[0]["gamets"] + 1;
     } else {
-        Logger::error("Empty recordset. ".__FILE__." ".__LINE__." ".__FUNCTION__);
+        Logger::error("Empty recordset. " . __FILE__ . " " . __LINE__ . " " . __FUNCTION__);
     }
 
     if (isset($GLOBALS["argv"][2])) {
-        if ($GLOBALS["argv"][2]=="instruction") {
+        if ($GLOBALS["argv"][2] == "instruction") {
             Logger::info("Loading instruction command");
             require_once("cmd" . DIRECTORY_SEPARATOR . "instruction.php");
-        } else if ($GLOBALS["argv"][2]=="suggestion") {
+        } else if ($GLOBALS["argv"][2] == "suggestion") {
             Logger::info("Loading suggestion command");
             require_once("cmd" . DIRECTORY_SEPARATOR . "suggestion.php");
-        } else if ($GLOBALS["argv"][2]=="impersonation") {
+        } else if ($GLOBALS["argv"][2] == "impersonation") {
             Logger::info("Loading impersonation command");
             require_once("cmd" . DIRECTORY_SEPARATOR . "impersonation.php");
-        }  else if ($GLOBALS["argv"][2]=="spawn") {
+        } else if ($GLOBALS["argv"][2] == "spawn") {
             Logger::info("Loading spawn command");
             require_once("cmd" . DIRECTORY_SEPARATOR . "spawncharacter.php");
-        }  else if ($GLOBALS["argv"][2]=="smart_impersonation") {
+        } else if ($GLOBALS["argv"][2] == "smart_impersonation") {
             Logger::info("Loading smart_impersonation command");
             require_once("cmd" . DIRECTORY_SEPARATOR . "smart_impersonation.php");
+        } else if ($GLOBALS["argv"][2] == "hypnosis") {
+            Logger::info("Loading hypnosis command");
+            require_once("cmd" . DIRECTORY_SEPARATOR . "hypnosis.php");
         }
     }
 
 
 }
-?>
+    ?>
